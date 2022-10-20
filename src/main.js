11294396d9351ef0fb61145c11d6930c4e2b0e33
k54kdk <3149827616@qq.com>
2022-10-20 18:29:59 +0800

    element-ui实现分分页控制# 5

--- src/main.js
+++ src/main.js
@@ -8,7 +8,6 @@ import './index.css'
 import './assets/css/login.css'
 import router from './router'
 // amap
-
 import VueAMap, { initAMapApiLoader } from '@vuemap/vue-amap';
 import '@vuemap/vue-amap/dist/style.css'
 // 创建 app 实例
@@ -17,8 +16,12 @@ const app = createApp(App)
 initAMapApiLoader({
     key: 'e09545015c0396aa7e7efd3467c11ed6'
 })
+// element-ui
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
 app.use(VueAMap)
 app.use(router)
-
+app.use(ElementPlus)
 // 挂载 app 实例
 app.mount('#app')
